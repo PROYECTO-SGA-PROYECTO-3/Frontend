56bc7ad6e09f437f1aecd47e943dd450ef59cb6e
neid-qm <neiderqm09@gmail.com>
2026-09-21 11:39:39 -0500

    feat(docentes): incorporar vistas de catalogo y formulario y registrar rutas de administracion

---
+++ src/features/docentes/index.ts
@@ -0,0 +1,7 @@
+export * from './types'
+export * from './api/docentesApi'
+export * from './hooks'
+export * from './components'
+
+export { default as DocentesPage } from './pages/DocentesPage'
+export { default as DocenteFormPage } from './pages/DocenteFormPage'
